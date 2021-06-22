3dd49bf04044a025369766d41a9ded480e1dc7ac
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2021-06-22 10:02:03 +0200

    Initial

---
+++ snake.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 22:46:06 2021
+
+@author: jonas
+"""
+
+class Snake:
+    """
+    This class is gonna control the snake and the gameboard. Each turn you call a method and tell the class where to move the snake. This class shall take care of collision detection, the game board, the position of the snake, spawning the apple and keeping track of the score. It should do everything except run the game loop, check the user input and render the game. This function should only run the backend. A seperate script shall collect the user input, render the game to the screen and implement the while loop to run the game.
+    """
+    
+    def __init__(self):
+        pass
+    
+    
+    
+if __name__ == "main":
+    #
+    #   Implement the while-loop, user input detection and rendering here. Maybe create a new class or some functions for that.
+    #
